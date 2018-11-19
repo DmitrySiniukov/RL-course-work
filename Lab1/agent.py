e5b1442f8e912286c3e33d67465269479a4c0258
yonk <nopctoday@gmail.com>
2018-11-19 16:20:49 +0100

    dp solution implemented

--- Lab1/agent.py
+++ Lab1/agent.py
@@ -2,12 +2,12 @@ import numpy as np
 from enum import Enum
 
 T = 15 # time limit
-map_width = 6
-map_height = 5
+map_width = 3
+map_height = 3
 map_size = (map_width, map_height)
 our_pos_zero = (0,0)
-his_pos_zero = (4,4)
-exit_pos = (4,4)
+his_pos_zero = (0,2)
+exit_pos = (2,2)
 
 class Step(Enum):
     NONE = (0,0)
@@ -77,17 +77,51 @@ class PartialState:
 
 
 class State:
-    def __init__(self, part_state, minotaur_pos):
+    def __init__(self, pos, minotaur_pos):
         self.minotaur_pos = minotaur_pos  # (x, y) tuple of the Minotaur's position
-        self.part_state = part_state
+        self.pos = pos
+
+    def listActions(self, pos = None):
+        if not pos: pos = self.pos
+        walls_dirs = walls.get(pos, [])
+        return [it for it in Direction if (it not in walls_dirs and in_map(self.step(pos, it)))]  # add the borders-check
+
+    def act(self, direction):
+        # Validity check
+        if direction not in self.listActions():
+            raise Exception("Illegal action!")
+
+        new_pos = self.step(self.pos, direction)
+        minotaur_dir = self.listActions(self.minotaur_pos)
+        minotaur_poses = [ self.step(self.minotaur_pos,dir) for dir in minotaur_dir if dir != Direction.NONE] # ignore stationary moves
+        minotaur_poses = [his_pos_zero]
+        # Generate a new state for each possible minotaur movement
+        return [State(new_pos, minotaur_pos) for minotaur_pos in minotaur_poses]
+
+    def step(cls, pos, direction):
+        if direction is Direction.NONE:
+            return (pos[0], pos[1])
+        if direction is Direction.LEFT:
+            return (pos[0] - 1, pos[1])
+        if direction is Direction.UP:
+            return (pos[0], pos[1] - 1)
+        if direction is Direction.RIGHT:
+            return (pos[0] + 1, pos[1])
+        return (pos[0], pos[1] + 1)
+
+    @property
+    def index_key(self):
+        return self.pos, self.minotaur_pos
+
 
 
 # One-way walls
-walls = { (1,0): [Direction.RIGHT], (1,1): [Direction.RIGHT], (1,2): [Direction.RIGHT],
-        (3,1): [Direction.RIGHT], (3,2): [Direction.RIGHT],
-        (4,1): [Direction.DOWN], (5,1): [Direction.DOWN],
-        (1,3): [Direction.DOWN], (2,3): [Direction.DOWN], (3,3): [Direction.DOWN], (4,3): [Direction.DOWN],
-        (3,4): [Direction.RIGHT] }
+# walls = { (1,0): [Direction.RIGHT], (1,1): [Direction.RIGHT], (1,2): [Direction.RIGHT],
+#         (3,1): [Direction.RIGHT], (3,2): [Direction.RIGHT],
+#         (4,1): [Direction.DOWN], (5,1): [Direction.DOWN],
+#         (1,3): [Direction.DOWN], (2,3): [Direction.DOWN], (3,3): [Direction.DOWN], (4,3): [Direction.DOWN],
+#         (3,4): [Direction.RIGHT] }
+walls = {}
 
 # New values are appended to the first (base) dictionary
 def append_to_dict(base_dict, new_vals):
@@ -144,10 +178,58 @@ def value_iteration_method():
             if new_delta > delta:
                 delta = new_delta
 
+steps = []
+counter = 0;
+visited = {}
+T = 6
+state_space_size = (T, *map_size, *map_size)
+V = np.ones(state_space_size) * -999
+def belman_eq(state:State, t):
+    global counter
+    counter += 1
+    #print(counter)
 
-def belman_eq(state, t):
-    our_pos, his_pos = state
+    #print("time:", t)
+    idx = (t, *state.pos, *state.minotaur_pos)
+    if t <= 0:
+        return 0 # Out of time
+    elif state.pos == state.minotaur_pos:
+        return -1 # Eaten
+    elif state.pos == exit_pos:
+        print('WIN')
+        return 1
+    elif V[idx] >= -1:
+         return V[idx]
 
-    # argmax
-    for direction in PartialState.getActions(our_pos, walls):
-        our_pos_new = PartialState.step(our_pos, direction)
+    # max action
+    v = V[idx]
+    dirs = state.listActions()
+    for direction in dirs:
+        next_states = state.act(direction)
+        p = 1. / len(next_states)
+
+        v_sum = 0
+        for nstate in next_states:
+            v_sum += belman_eq(nstate, t-1)
+
+        v_sum *= p # normalize
+
+        if v_sum > v:
+            v = v_sum
+            steps[t] = direction
+
+
+    V[idx] = v
+    return v
+
+
+def main():
+    init_state = State(our_pos_zero, his_pos_zero)
+    global steps
+    steps = [0] * T
+    belman_eq(init_state, T-1)
+    print(steps)
+    print('hello world')
+
+if __name__== "__main__":
+  main()
